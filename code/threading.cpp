215f347a0ab328e973a447678a7a7f9b3e8f811c
Israel David <israelcaetano21@hotmail.com>
2022-08-16 11:50:36 -0300

    Add main code: include libraries and main scope

---
+++ code/threading.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <p.thread>
+#include <semaphore.h>
+
+pthread_t thread_1;
+pthread_t thread_2;
+pthread_t thread_3;
+sem_t simple_semaphore;
+
+using namespace std; //takes away the need for using "std::"
+
+int main(){
+
+
+  return 0;
+}
